31ddef3f1bf32154c1e16a686e81e3fd9752bbc5
Felix Wang <fwang356@gatech.edu>
2021-06-29 20:52:14 -0500

    Responds to music bots when queued certain keyworks

--- src/bot.py
+++ src/bot.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import discord
 import random
@@ -11,24 +12,35 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
 
     print(
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
 
-responses = ["SHUT UP JOSH!!!!!!", "YO MAMA", "nice one", "so mean", "334-202-0003"]
+responses = ["SHUT UP JOSH!!!!!!", "YO MAMA!!!!!!", "NICE ONE!!!!!!", "SO MEAN!!!!!!", "THAT'S RESPECT!!!!!!"]
+phrases = ['mama', 'mom', 'mother']
 
 
 @client.event
 async def on_message(message):
-    if message.author.id != 403745233507975169:
+    if message.content[0:2] == '!p' or message.content[0:2] == '-p':
+        for s in phrases:
+            if s in message.content.lower():
+                await message.reply('STOP PLAYING YO MAMA JOKES!!!!!!')
+                await asyncio.sleep(1)
+                for members in message.author.voice.channel.members:
+                    if members.id == 235088799074484224 or members.id == 234395307759108106:
+                        await members.edit(voice_channel=None)
+                        return
+    if message.author.id != 404383046733791233:
         return
     else:
-        if random() * 100 > 69:
-            await message.reply(responses[random.randrange(0, responses.len)])
+        rand = random.random() * 100
+        if rand > 92:
+            await message.reply(file=discord.File('C:/Users/crayo/Anti_Troll_Discord_Bot/facebank.jpg'))
+        elif rand > 65:
+            await message.reply(responses[random.randrange(0, len(responses))])
 
 client.run(TOKEN)
